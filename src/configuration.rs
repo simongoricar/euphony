65239491047867dd576d9bc31b1e80771f0b6053
Simon Goričar <simon.peter.goricar@gmail.com>
2022-06-18 15:28:27 +0200

    Validation command kind of works

--- src/configuration.rs
+++ src/configuration.rs
@@ -8,7 +8,14 @@ use serde::{Deserialize};
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub basics: ConfigBasics,
+    pub validation: ConfigValidation,
     pub libraries: HashMap<String, ConfigLibrary>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ConfigValidation {
+    pub audio_file_extensions: Vec<String>,
+    pub ignored_file_extensions: Vec<String>,
 }
 
 #[derive(Deserialize, Debug)]
@@ -20,6 +27,9 @@ pub struct ConfigBasics {
 pub struct ConfigLibrary {
     pub name: String,
     pub path: String,
+
+    pub audio_file_extensions: Vec<String>,
+    pub must_not_contain_extensions: Vec<String>,
 }
 
 
@@ -35,36 +45,61 @@ pub fn get_configuration_file_path() -> String {
     )
 }
 
-pub fn load_configuration(configuration_filepath: String) -> Config {
-    // Read the configuration file into memory.
-    let configuration_string = fs::read_to_string(configuration_filepath)
-        .expect("Could not read configuration file!");
+impl Config {
+    pub fn load_from_path(configuration_filepath: String) -> Config {
+        // Read the configuration file into memory.
+        let configuration_string = fs::read_to_string(configuration_filepath)
+            .expect("Could not read configuration file!");
 
-    // Parse the string into a structure.
-    let mut config: Config = toml::from_str(&configuration_string)
-        .expect("Could not load configuration file!");
+        // Parse the string into a structure.
+        let mut config: Config = toml::from_str(&configuration_string)
+            .expect("Could not load configuration file!");
 
-    // Parse paths inside the configuration before returning.
-    for (_, mut library) in config.libraries.borrow_mut() {
-        library.path = library.path.replace(
-            "{ROOT}",
-            &config.basics.root_library_path
-        );
-
-        // Ensure the path is valid
-        let true_path = Path::new(&library.path);
-        if !true_path.exists() {
-            panic!("Library \"{}\" does not exist (path: \"{}\")!", library.name, library.path);
+        // Parse extensions to be lowercase
+        for ext in config.validation.audio_file_extensions.iter_mut() {
+            ext.make_ascii_lowercase();
         }
+
+        for ext in config.validation.ignored_file_extensions.iter_mut() {
+            ext.make_ascii_lowercase();
+        }
+
+        // Parse paths inside the configuration before returning.
+        for (_, mut library) in config.libraries.borrow_mut() {
+            library.path = library.path.replace(
+                "{ROOT}",
+                &config.basics.root_library_path
+            );
+
+            // Ensure the path is valid
+            let true_path = Path::new(&library.path);
+            if !true_path.exists() {
+                panic!("Library \"{}\" does not exist (path: \"{}\")!", library.name, library.path);
+            }
+
+            // Make extensions lowercase
+            for ext in library.audio_file_extensions.iter_mut() {
+                ext.make_ascii_lowercase();
+            }
+
+            for ext in library.must_not_contain_extensions.iter_mut() {
+                ext.make_ascii_lowercase();
+            }
+        }
+
+        config
     }
 
-    config
-}
-
-pub fn print_libraries(libraries: HashMap<String, ConfigLibrary>) {
-    println!("Available libraries:");
-    for (_, library) in libraries.borrow() {
-        println!("  {}: {}", library.name, library.path);
+    pub fn load() -> Config {
+        let configuration_filepath = get_configuration_file_path();
+        Config::load_from_path(configuration_filepath)
     }
-    println!();
+
+    pub fn print_libraries(&self) {
+        println!("Available libraries:");
+        for (_, library) in self.libraries.borrow() {
+            println!("  {}: {}", library.name, library.path);
+        }
+        println!();
+    }
 }
---
+++ src/filesystem.rs
@@ -0,0 +1,57 @@
+use std::fs;
+use std::fs::DirEntry;
+use std::io::{Error, ErrorKind};
+use std::path::Path;
+
+
+/// Given a PathBuf scan its contents and
+/// return a Result: when Ok it is a tuple of two Vec<DirEntry> elements,
+/// the first one containing the files, the second one directories.
+pub fn list_directory_contents(directory: &Path)
+    -> Result<(Vec<DirEntry>, Vec<DirEntry>), Error> {
+    let mut file_list: Vec<DirEntry> = Vec::new();
+    let mut directory_list: Vec<DirEntry> = Vec::new();
+
+    let dir_read = fs::read_dir(directory)?;
+
+    for entry in dir_read {
+        let entry = entry?;
+        let entry_type = entry.file_type()?;
+
+        if entry_type.is_file() {
+            file_list.push(entry);
+        } else if entry_type.is_dir() {
+            directory_list.push(entry);
+        } else {
+            // Skip other types.
+            continue;
+        }
+    }
+
+    Ok((file_list, directory_list))
+}
+
+pub fn list_dir_entry_contents(dir_entry: &DirEntry)
+    -> Result<(Vec<DirEntry>, Vec<DirEntry>), Error> {
+    let path = dir_entry.path();
+
+    return if !path.is_dir() {
+        Err(Error::new(ErrorKind::Other, "dir_entry is not a directory."))
+    } else {
+        list_directory_contents(path.as_path())
+    }
+
+}
+
+/// Given a DirEntry, get its file extension and return it in a Result<String, ()>.
+pub fn get_dir_entry_file_extension(dir_entry: &DirEntry) -> Result<String, Error> {
+    match dir_entry.path().extension() {
+        Some(extension) => {
+            match extension.to_os_string().into_string() {
+                Ok(ext) => Ok(ext.to_ascii_lowercase()),
+                Err(_) => Err(Error::new(ErrorKind::Other, "Could not get file extension.")),
+            }
+        },
+        None => Err(Error::new(ErrorKind::Other, "Could not get file extension.")),
+    }
+}
